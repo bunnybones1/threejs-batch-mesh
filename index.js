a7eed7c3e41e97beeadc8056afd2bfcba18c92c3
Tomasz Dysinski <oz@bunnybones.com>
2019-01-02 17:36:32 -0500

    batching meshes together and maintaining the attribute buffers

--- index.js
+++ index.js
@@ -1,5 +1,186 @@
-function Batching() {
+var three = require('three');
+var extend = require('extend-shallow');
 
+/*
+    consider using a shadow scene instead of making the source meshes invisible
+
+*/
+
+function BatchingMesh(scene, material) {
+    this.scene = scene;
+    var geometry = new three.BufferGeometry();
+    this.material = material;
+    this.meshes = [];
+    three.Mesh.call(this, geometry, material);
+    this.supportedAttributes = [];
+    this.supportedAttributeSizes = {};
+
+    this.dirtyPositions = false;
+    this.dirtyIndices = false;
 }
 
-module.exports = Batching;
+var tempVec3 = new three.Vector3;
+function copyAndTransformPositionBuffer(dstArr, srcArr, offset, matrixWorld) {
+    for(var i = 0; i < offset; i += 3) {
+        tempVec3.x = srcArr[i];
+        tempVec3.y = srcArr[i+1];
+        tempVec3.z = srcArr[i+2];
+        tempVec3.applyMatrix4(matrixWorld);
+        dstArr[offset+i] = tempVec3.x;
+        dstArr[offset+i+1] = tempVec3.y;
+        dstArr[offset+i+2] = tempVec3.z;
+    }
+    // debugger;
+}
+function copyAndTransformNormalBuffer(dstArr, srcArr, offset, matrixWorld) {
+    var normalMatrix = new three.Matrix3().getNormalMatrix( matrixWorld );
+    for(var i = 0; i < offset; i += 3) {
+        tempVec3.x = srcArr[i];
+        tempVec3.y = srcArr[i+1];
+        tempVec3.z = srcArr[i+2];
+        tempVec3.applyMatrix3(normalMatrix);
+        dstArr[offset+i] = tempVec3.x;
+        dstArr[offset+i+1] = tempVec3.y;
+        dstArr[offset+i+2] = tempVec3.z;
+    }
+}
+
+function copyAttrBufferArray(name, dstArr, srcArr, offset, matrixWorld) {
+    switch(name) {
+        case "position": 
+            return copyAndTransformPositionBuffer(dstArr, srcArr, offset, matrixWorld);
+        case "normal": 
+            return copyAndTransformNormalBuffer(dstArr, srcArr, offset, matrixWorld);
+        default: 
+            dstArr.set(srcArr, offset);
+    }
+}
+
+function onEnterFrame() {
+    var that = this;
+    if(!this.dirtyPositions) {
+        this.dirtyPositions = this.meshes.some(m => {
+            return !(m.positionLast.equals(m.position) && m.quaternionLast.equals(m.quaternion) && m.scaleLast.equals(m.scale));
+        });
+    }
+    if(!this.dirtyNormals) {
+        this.dirtyNormals = this.meshes.some(m => {
+            return !m.quaternionLast.equals(m.quaternion);
+        });
+        this.dirtyNormals = true;
+    }
+
+    this.meshes.forEach(m => {
+        m.positionLast.copy(m.position)
+        m.quaternionLast.copy(m.quaternion)
+        m.scaleLast.copy(m.scale);
+    });
+
+    if(!this.dirtyPositions && !this.dirtyNormals && !this.dirtyIndices) return;
+
+    var meshes = this.meshes;
+    var geometry = this.geometry;
+    var sa = this.supportedAttributes;
+    var sas = this.supportedAttributeSizes;
+    if(this.dirtyPositions || this.dirtyNormals || this.dirtyAll) {
+
+        meshes.forEach( m => m.updateMatrixWorld());
+
+        //recalculate buffer sizes
+        sa.forEach(n => {
+            sas[n] = meshes.reduce((total, mesh) => {
+                return total + mesh.geometry.attributes[n].count;
+            }, 0);
+        });
+
+        //allocate new sizes if necessary
+        sa.forEach(n => {
+            var attribute = geometry.attributes[n];
+            if(attribute.count != sas[n]) {
+                attribute.array = new attribute.array.constructor(sas[n] * attribute.itemSize);
+                attribute.count = sas[n];
+            }
+        });
+
+        //populate with data
+        var cursor = 0;
+        sa.forEach(n => {
+            if((n == "position" && that.dirtyPositions) || (n == "normal" && that.dirtyNormals) || this.dirtyAll) {
+                cursor = 0;
+                var attribute = geometry.attributes[n];
+                for(var i = 0; i < meshes.length; i++) {
+                    var mesh = meshes[i];
+                    var srcAttr = mesh.geometry.attributes[n];
+                    copyAttrBufferArray(n, attribute.array, srcAttr.array, cursor, mesh.matrixWorld);
+                    cursor += srcAttr.array.length;
+                }
+                attribute.needsUpdate = true;
+            }
+        });
+        that.dirtyPositions = false;
+        that.dirtyNormals = false;
+    }
+
+    if(this.dirtyIndices) {
+        var totalIndices = meshes.reduce( (total, mesh) => {
+            return total + mesh.geometry.index.count;
+        }, 0);
+        if( geometry.index.count != totalIndices) {
+            geometry.index.array = new geometry.index.array.constructor(totalIndices);
+            geometry.index.count = totalIndices;
+        }
+
+        cursor = 0;
+        cursorPositions = 0;
+        meshes.forEach(mesh => {
+            var dstArr = geometry.index.array;
+            var srcArr = mesh.geometry.index.array;
+            for(var i = 0; i < srcArr.length; i++) {
+                dstArr[cursor+i] = srcArr[i] + cursorPositions;
+            }
+            cursor += srcArr.length;
+            cursorPositions += mesh.geometry.attributes.position.count;
+        });
+
+        geometry.index.needsUpdate = true;
+
+        this.dirtyIndices = false;
+    }
+
+    this.dirtyAll = false;
+}
+
+function cloneEmptyAttribute(src, size = 1) {
+    return new src.constructor(new src.array.constructor(size * src.itemSize), src.itemSize);
+}
+
+function addToBatch(mesh) {
+    if(this.meshes.length == 0) {
+        this.supportedAttributes = Object.keys(mesh.geometry.attributes);
+        var sas = this.supportedAttributeSizes;
+        var geometry = this.geometry;
+        this.supportedAttributes.forEach(n => {
+            sas[n] = 0;
+            geometry.addAttribute(n, cloneEmptyAttribute(mesh.geometry.attributes[n]));
+        });
+        geometry.setIndex(cloneEmptyAttribute(mesh.geometry.index));
+    }
+    if(mesh.material !== this.material) throw new Error("Cannot batch Meshes that use different materials.");
+    this.meshes.push(mesh);
+    mesh.positionLast = mesh.position.clone();
+    mesh.quaternionLast = mesh.quaternion.clone();
+    mesh.scaleLast = mesh.scale.clone();
+    mesh.visible = false;
+    this.dirtyPositions = true;
+    this.dirtyIndices = true;
+    this.dirtyAll = true;
+}
+
+BatchingMesh.prototype = Object.create(three.Mesh.prototype);
+
+extend(BatchingMesh.prototype, {
+    onEnterFrame,
+    addToBatch
+});
+
+module.exports = BatchingMesh;
--- test.js
+++ test.js
@@ -1,16 +1,23 @@
-var THREE = require('three');
-// require('./utils/threeSafety');
+var THREE = require("three");
+// require("./utils/threeSafety");
 window.THREE = THREE;
-var ManagedView = require('threejs-managed-view');
-var Batching = require('./');
-var urlparam = require('urlparam');
+var ManagedView = require("threejs-managed-view");
+var BatchingMesh = require("./");
+var urlparam = require("urlparam");
 var view = new ManagedView.View({
 	skipFrames: 10
 });
 
+
 var debugOverdraw = false;
 //lights
-var light = new THREE.PointLight(0xffffff, 0.7);
+var light = new THREE.SpotLight(0xffffff, 0.7, 50, 45);
+
+light.castShadow = true;
+
+light.shadow = new THREE.LightShadow( new THREE.PerspectiveCamera( 50, 1, 1, 150 ) );
+light.shadow.bias = 0.0001;
+
 light.position.x += 100;
 light.position.y += 100;
 view.scene.add(light);
@@ -25,10 +32,15 @@ var matParams = {
 };
 var mat = new THREE.MeshPhongMaterial(matParams);
 
+view.renderer.shadowMap.enabled = true;
+view.renderer.shadowMap.type = THREE.PCFShadowMap;
 view.renderer.setClearColor(debugOverdraw ? 0x000000 : 0xdfefef, 1);
 
+var helper = new THREE.CameraHelper( light.shadow.camera );
+view.scene.add( helper );
+
 var centerOfView = new THREE.Vector3();
-var totalBalls = urlparam('totalBalls', 20);
+var totalBalls = urlparam("totalBalls", 20);
 
 function rand(scale = 10) { return (Math.random() - 0.5) * scale; }
 function randAngle(scale = 1) { return Math.random() * Math.PI * 2; }
@@ -40,16 +52,22 @@ function randQuat(scale = 0.1) {
 	return quat;
 }
 
+var batchingMesh = new BatchingMesh(view.scene, mat);
+// view.scene.add(batchingMesh);
+
 var balls = [];
+var geom = new THREE.SphereBufferGeometry(1, 8, 4);
 for(var i = 0; i < totalBalls; i++) {
-	var ball = new THREE.Mesh(new THREE.SphereGeometry(), mat);
+	var ball = new THREE.Mesh(geom, mat);
+	ball.castShadow = true;
+	ball.recieveShadow = true;
+	
 	balls.push(ball);
 	ball.position.set(rand(), rand(), rand());
 	ball.rotation.set(randAngle(), randAngle(), randAngle());
-	centerOfView.add(ball.position);
 	view.scene.add(ball);
+	// batchingMesh.addToBatch(ball);
 }
-centerOfView.multiplyScalar(1 / totalBalls);
 
 view.camera.position.x += 13;
 view.camera.position.y += 2;
@@ -68,18 +86,22 @@ function onEnterFrame() {
 	}
 
 	centerOfView.set(0, 0, 0);
+	var now = Date.now();
 	for(var i = 0; i < totalBalls; i++) {
 		var ball = balls[i];
-		ball.position.multiplyScalar(0.99);
-		ball.translateZ(0.1);
-		ball.applyQuaternion(randQuat(0.1));
+		if(((now * 0.001) % 4) > 2) {
+			ball.position.multiplyScalar(0.99);
+			ball.translateZ(0.1);
+			ball.applyQuaternion(randQuat(0.1));
+		}
 		// ball.position.set(rand(), rand(), rand());
 		centerOfView.add(ball.position);
 	}
 	centerOfView.multiplyScalar(1 / totalBalls);
 	
 	//put light and camera focus in the center of gravity
-	light.position.y += 10;
+	light.position.copy(centerOfView);
+	light.position.y += 30;
 	var delta = view.camera.position.clone().sub(centerOfView);
 	var angle = Math.atan2(delta.z, delta.x);
 	angle += 0.002;
@@ -92,8 +114,8 @@ function onEnterFrame() {
 view.renderManager.onEnterFrame.add(onEnterFrame);
 
 var RafTweener = require("raf-tweener");
-var Pointers = require('input-unified-pointers');
-var MouseWheel = require('input-mousewheel');
+var Pointers = require("input-unified-pointers");
+var MouseWheel = require("input-mousewheel");
 var CameraController = require("threejs-camera-controller-pan-zoom-unified-pointer");
 
 var pointers = new Pointers(view.canvas);
@@ -132,6 +154,7 @@ setOtherCameraSize(window.innerWidth, window.innerHeight);
 view.onResizeSignal.add(setOtherCameraSize);
 
 view.renderManager.onEnterFrame.add(function(){
+	batchingMesh.onEnterFrame();
 	camController.precomposeViewport(otherCamera);
 });
-view.renderManager.skipFrames = urlparam('skipFrames', 0);
+view.renderManager.skipFrames = urlparam("skipFrames", 0);
